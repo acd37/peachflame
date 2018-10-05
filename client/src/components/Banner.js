0d23115ff7275034b61425861380b0aebcef1406
acd37 <alecdown@gmail.com>
2018-10-05 10:40:01 -0600

    front end ui updates to titles and banner

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,28 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Typography } from "@material-ui/core";
 
 const styles = {
   appHeader: {
     background: "linear-gradient(to left, #00c6ff, #0072ff)",
     marginTop: 80,
-    height: 150,
+    height: 30,
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center"
   },
   headerText: {
-    color: "#fff",
-    fontSize: "3em"
+    fontSize: "3em",
+    marginTop: 40
   }
 };
 
 class Banner extends Component {
   render() {
     return (
-      <div style={styles.appHeader}>
-        <Typography style={styles.headerText}>{this.props.title}</Typography>
-      </div>
+      <Fragment>
+        <div style={styles.appHeader} />
+        <Typography variant="display4" style={styles.headerText}>
+          {this.props.title}
+        </Typography>
+      </Fragment>
     );
   }
 }
