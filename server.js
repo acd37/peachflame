2455216eb42a4ddb1ce79587d342bfe14da18a17
Alec Down <alecdown@gmail.com>
2020-01-05 09:31:51 -0600

    adds nodejs compression module

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const express = require('express');
-require('dotenv').config()
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path');
-const chalk = require('chalk');
-const { morganConfig } = require('./config/morganConfig');
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+const chalk = require("chalk");
+const compression = require("compression");
+const { morganConfig } = require("./config/morganConfig");
 
-const users = require('./routes/api/users');
-const projects = require('./routes/api/projects');
-const email = require('./routes/api/email');
-
+const users = require("./routes/api/users");
+const projects = require("./routes/api/projects");
+const email = require("./routes/api/email");
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+// compression
+app.use(compression());
 
 // Bodyparser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // morgan logging
 
@@ -28,29 +31,31 @@ app.use(morganConfig);
 
 // Connect to MongoDB
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB connected.'))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected."))
+  .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/projects', projects);
-app.use('/api/email', email);
+app.use("/api/users", users);
+app.use("/api/projects", projects);
+app.use("/api/email", email);
 
 // server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-app.listen(port, () => console.log(`Server running on port ${chalk.green.bold(port)}!`));
+app.listen(port, () =>
+  console.log(`Server running on port ${chalk.green.bold(port)}!`)
+);
